c9b304a0375509d7d674f90ebec5977c2f597993
Liès TIGUERCHA <lies.tiguercha@decathlon.com>
2024-02-26 22:44:21 +0100

    push

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # fablocker-vocale-assistant
+
+How to use:
+
+1. Install Python on your machine, then  proceed with the requirements:
+
+```bash
+pip install -r requirements.txt
+```
+
+2. Install Cookie Editor extension in your browser.
+
+![img.png](img.png)
+
+3. Go to https://www.bing.com/ and copy the cookies with the extension.
+   
+![img_2.png](img_2.png)
+
+4. Now, create a file named `cookies.json` and paste the cookies from the extension.
---
+++ main.py
@@ -0,0 +1,76 @@
+import asyncio
+import json
+import os
+
+import pyttsx3
+import speech_recognition as sr
+from EdgeGPT.EdgeGPT import Chatbot, ConversationStyle
+
+recognizer = sr.Recognizer()
+speak = pyttsx3.init()
+speak.setProperty('voice', 'french')
+rate = speak.getProperty('rate')
+speak.setProperty('rate', rate - 50)
+volume = speak.getProperty('volume')
+speak.setProperty('volume', volume + 0.50)
+voices = speak.getProperty('voices')
+speak.setProperty('voice', voices[0].id)
+
+
+def talk(text):
+    speak.say(text)
+    speak.runAndWait()
+
+
+def command():
+    action = ""
+    try:
+        with sr.Microphone() as mic:
+            voice = recognizer.listen(mic)
+            action = recognizer.recognize_google(voice)
+            action = action.lower()
+    except:
+        pass
+    return action
+
+
+async def ask_bing():
+    print("Ouverture de Bing AI")
+    cookies = json.loads(open("./cookies.json", encoding="utf-8").read())
+    while True:
+        bot = await Chatbot.create(cookies=cookies)
+        response = await bot.ask(prompt=input("Demande quelque chose à bing AI : "),
+                                 conversation_style=ConversationStyle.creative)
+        for message in response["item"]["messages"]:
+            if message["author"] == "bot":
+                bot_response = message["text"]
+        print("Bot's reponse:", bot_response)
+        await bot.close()
+
+
+def run():
+    speak.say("Hello, how can I help ?")
+    print("Hello, how can I help ?")
+    while True:
+        action = command()
+        print(action)
+        if ("google chrome") in action:
+            os.system("start chrome")
+            speak.say("I open chrome")
+            speak.runAndWait()
+        elif ("notepad") in action:
+            os.system("notepad")
+            speak.say("I'm opening the notepad")
+            speak.runAndWait()
+        elif ("chat") in action:
+            asyncio.run(ask_bing())
+        elif ("stop") in action:
+            speak.say("Good bye")
+            speak.runAndWait()
+            exit()
+        else:
+            print("I did not understand")
+
+
+if __name__ == "__main__":
+    run()
